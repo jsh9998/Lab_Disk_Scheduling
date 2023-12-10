2fc5df10e9839bc24f7f3215447f98b0981c391a
ProgramHenry <jsh9998@nyu.edu>
2023-12-10 16:11:17 -0600

    test
    test

--- disk.c
+++ disk.c
@@ -32,3 +32,24 @@ struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *
     (*queue_cnt)++;
     return current_request;
 };
+
+struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt)
+{
+    return createNullRCB();
+};
+struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp)
+{
+    return createNullRCB();
+};
+struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder)
+{
+    return createNullRCB();
+};
+struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp)
+{
+    return createNullRCB();
+};
+struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder, int scan_direction)
+{
+    return createNullRCB();
+};
