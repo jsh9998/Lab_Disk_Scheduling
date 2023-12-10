e9e16dc124846d0767674297ccd20599c0ec95ec
ProgramHenry <jsh9998@nyu.edu>
2023-12-10 16:09:21 -0600

    test
    test

--- disk.c
+++ disk.c
@@ -1,1 +1,34 @@
 #include "oslabs.h"
+#define TRUE 1
+#define FALSE 0
+
+struct RCB createNullRCB();
+struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
+struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt);
+struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
+struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder);
+struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
+struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder, int scan_direction);
+
+struct RCB createNullRCB()
+{
+    struct RCB nullRCB;
+    nullRCB.request_id = 0;
+    nullRCB.arrival_timestamp = 0;
+    nullRCB.cylinder = 0;
+    nullRCB.address = 0;
+    nullRCB.process_id = 0;
+    return nullRCB;
+};
+
+struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp)
+{
+    if (current_request.process_id == 0)
+    {
+        return new_request;
+    }
+
+    request_queue[*queue_cnt] = new_request;
+    (*queue_cnt)++;
+    return current_request;
+};
