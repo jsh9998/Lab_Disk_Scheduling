4792c654f37904dd3c8b8637e177c4dbf54890e3
ProgramHenry <jsh9998@nyu.edu>
2023-12-10 17:21:08 -0600

    test
    test

--- disk.c
+++ disk.c
@@ -4,6 +4,8 @@
 #define FALSE 0
 
 struct RCB createNullRCB();
+int find_next_rcb_down(struct RCB request_queue[QUEUEMAX], int queue_cnt, int current_cylinder);
+int find_next_rcb_up(struct RCB request_queue[QUEUEMAX], int queue_cnt, int current_cylinder);
 struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
 struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt);
 struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
